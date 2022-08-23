8e9bbddd887ffcf1e4b3d03be73b46a387ce1cad
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-08-23 18:16:20 +0200

    add Proudct Page

--- script/main.js
+++ script/main.js
@@ -55,26 +55,104 @@ check.forEach((item) => {
   });
 });
 
-// List Style
-let list = document.querySelectorAll(".list");
-let list1 = document.querySelector(".list-1");
-let list2 = document.querySelector(".list-2");
-
-let container = document.querySelector(".products div");
-list.forEach((li) => {
-  li.addEventListener("click", ActiveContorl);
+// Input Number
+let Plus = document.querySelector(".plus");
+let minus = document.querySelector(".minus");
+let num = document.querySelector(".numb");
+let Number = 1;
+Plus.addEventListener("click", () => {
+  Number++;
+  Number = Number < 10 ? "0" + Number : Number;
+  num.innerHTML = `${Number}`;
 });
-function ActiveContorl() {
-  list.forEach((li) => {
+minus.addEventListener("click", () => {
+  if (Number > 1) {
+    Number--;
+    Number = Number < 10 ? "0" + Number : Number;
+    num.innerHTML = `${Number}`;
+  }
+});
+// Description
+let discList = document.querySelectorAll(".discList li");
+let discBtn = document.getElementById("description");
+let addBtn = document.getElementById("additonal");
+let revBtn = document.getElementById("reviews");
+let f1 = document.querySelector(".f1");
+let f2 = document.querySelector(".f2");
+discList.forEach((li) => {
+  console.log("first");
+  li.addEventListener("click", removeActiveDescripton);
+});
+function removeActiveDescripton() {
+  discList.forEach((li) => {
     li.classList.remove("active");
     this.classList.add("active");
   });
 }
-list1.addEventListener('click',()=>{
-  container.classList.remove("rowProducts");
-  container.classList.add("content");
-})
-list2.addEventListener('click',()=>{
-  container.classList.remove("content");
-  container.classList.add("rowProducts");
-})
+
+discBtn.addEventListener("click", () => {
+  f1.innerHTML =
+    "Ratione volurtatem serui nesciunt neaue porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.";
+  f2.innerHTML =
+    "Quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.";
+});
+addBtn.addEventListener("click", () => {
+  f1.innerHTML =
+    "Some industries mandate current certifications to even be considered for employment. If that is your field, a separate certification category can make it easy for an interviewer to scan your resume and find your current licenses and certifications quickly. If you have a non-mandatory certification as part of your personal professional development, you can explain the organization issuing the certification and what coursework and testing it required.";
+  f2.innerHTML =
+    "klam klam klam klam bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ";
+});
+revBtn.addEventListener("click", () => {
+  f1.innerHTML =
+    "Getting customers to write positive reviews about your company is very beneficial, but it can also be challenging.";
+  f2.innerHTML =
+    "That’s why you should take some action to encourage your customers to write reviews. One of the ways to motivate them is to show them positive examples for your industry.";
+});
+/*
+https://www.facebook.com/sharer.php?u=[post-url]
+.
+https://twitter.com/share?url=[post-url]&text=[post-title]
+.
+https://pinterest.com/pin/create/bookmarklet/?media=[post-img]&url=[post-url]&is_video=[is_video]&description=[post-title]
+.
+https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+.
+https://api.whatsapp.com/send?text=[post-title] [post-url]
+
+*/
+// Share
+let facebookBtn = document.querySelector(".btn-facebook");
+let twitterBtn = document.querySelector(".btn-twitter");
+let linkedintn = document.querySelector(".btn-linkedin");
+let pinterestBtn = document.querySelector(".btn-pinterest");
+let whatsappBtn = document.querySelector(".btn-whatsapp");
+
+function share() {
+  const pinterestImg = document.querySelector(".pinterestImg");
+  let PostUrl = encodeURI(document.location.href);
+  let postTitle = encodeURI(
+    "Hi everyone, I had a pleasant experience with this site and I share with you this masterpiece 🤍❤️"
+  );
+  let postImg = encodeURI(pinterestImg.src);
+  facebookBtn.setAttribute(
+    "href",
+    `https://www.facebook.com/sharer.php?u=${PostUrl}`
+  );
+  twitterBtn.setAttribute(
+    "href",
+    `https://twitter.com/share?url=${PostUrl}&text=${postTitle}`
+  );
+  linkedintn.setAttribute(
+    "href",
+    `https://www.linkedin.com/shareArticle?url=${PostUrl}&title=${postTitle}`
+  );
+  pinterestBtn.setAttribute(
+    "href",
+    `https://pinterest.com/pin/create/bookmarklet/?media=${postImg}&url=${PostUrl}&description=${postTitle}`
+  );
+  whatsappBtn.setAttribute(
+    "href",
+    `https://api.whatsapp.com/send?text=${postTitle} ${PostUrl}`
+  );
+}
+share();

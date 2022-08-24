26f905c0b2211acb89ae5ca80c10738f5bb3f1c8
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-08-24 09:47:19 +0200

    Fix addEventListener Error

--- script/main.js
+++ script/main.js
@@ -60,18 +60,22 @@ let Plus = document.querySelector(".plus");
 let minus = document.querySelector(".minus");
 let num = document.querySelector(".numb");
 let Number = 1;
-Plus.addEventListener("click", () => {
-  Number++;
-  Number = Number < 10 ? "0" + Number : Number;
-  num.innerHTML = `${Number}`;
-});
-minus.addEventListener("click", () => {
-  if (Number > 1) {
-    Number--;
+if (Plus) {
+  Plus.addEventListener("click", () => {
+    Number++;
     Number = Number < 10 ? "0" + Number : Number;
     num.innerHTML = `${Number}`;
-  }
-});
+  });
+}
+if (minus) {
+  minus.addEventListener("click", () => {
+    if (Number > 1) {
+      Number--;
+      Number = Number < 10 ? "0" + Number : Number;
+      num.innerHTML = `${Number}`;
+    }
+  });
+}
 // Description
 let discList = document.querySelectorAll(".discList li");
 let discBtn = document.getElementById("description");
@@ -88,25 +92,30 @@ function removeActiveDescripton() {
     this.classList.add("active");
   });
 }
-
-discBtn.addEventListener("click", () => {
-  f1.innerHTML =
-    "Ratione volurtatem serui nesciunt neaue porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.";
-  f2.innerHTML =
-    "Quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.";
-});
-addBtn.addEventListener("click", () => {
-  f1.innerHTML =
-    "Some industries mandate current certifications to even be considered for employment. If that is your field, a separate certification category can make it easy for an interviewer to scan your resume and find your current licenses and certifications quickly. If you have a non-mandatory certification as part of your personal professional development, you can explain the organization issuing the certification and what coursework and testing it required.";
-  f2.innerHTML =
-    "klam klam klam klam bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ";
-});
-revBtn.addEventListener("click", () => {
-  f1.innerHTML =
-    "Getting customers to write positive reviews about your company is very beneficial, but it can also be challenging.";
-  f2.innerHTML =
-    "That’s why you should take some action to encourage your customers to write reviews. One of the ways to motivate them is to show them positive examples for your industry.";
-});
+if (discBtn) {
+  discBtn.addEventListener("click", () => {
+    f1.innerHTML =
+      "Ratione volurtatem serui nesciunt neaue porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.";
+    f2.innerHTML =
+      "Quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.";
+  });
+}
+if (addBtn) {
+  addBtn.addEventListener("click", () => {
+    f1.innerHTML =
+      "Some industries mandate current certifications to even be considered for employment. If that is your field, a separate certification category can make it easy for an interviewer to scan your resume and find your current licenses and certifications quickly. If you have a non-mandatory certification as part of your personal professional development, you can explain the organization issuing the certification and what coursework and testing it required.";
+    f2.innerHTML =
+      "klam klam klam klam bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ";
+  });
+}
+if (revBtn) {
+  revBtn.addEventListener("click", () => {
+    f1.innerHTML =
+      "Getting customers to write positive reviews about your company is very beneficial, but it can also be challenging.";
+    f2.innerHTML =
+      "That’s why you should take some action to encourage your customers to write reviews. One of the ways to motivate them is to show them positive examples for your industry.";
+  });
+}
 /*
 https://www.facebook.com/sharer.php?u=[post-url]
 .
@@ -122,7 +131,7 @@ https://api.whatsapp.com/send?text=[post-title] [post-url]
 // Share
 let facebookBtn = document.querySelector(".btn-facebook");
 let twitterBtn = document.querySelector(".btn-twitter");
-let linkedintn = document.querySelector(".btn-linkedin");
+let linkedinBtn = document.querySelector(".btn-linkedin");
 let pinterestBtn = document.querySelector(".btn-pinterest");
 let whatsappBtn = document.querySelector(".btn-whatsapp");
 
@@ -133,25 +142,30 @@ function share() {
     "Hi everyone, I had a pleasant experience with this site and I share with you this masterpiece 🤍❤️"
   );
   let postImg = encodeURI(pinterestImg.src);
+  if(facebookBtn){
   facebookBtn.setAttribute(
     "href",
     `https://www.facebook.com/sharer.php?u=${PostUrl}`
-  );
+  );}
+  if(twitterBtn){
   twitterBtn.setAttribute(
     "href",
     `https://twitter.com/share?url=${PostUrl}&text=${postTitle}`
-  );
-  linkedintn.setAttribute(
+  );}
+  if(linkedinBtn){
+  linkedinBtn.setAttribute(
     "href",
     `https://www.linkedin.com/shareArticle?url=${PostUrl}&title=${postTitle}`
-  );
+  );}
+  if(pinterestBtn){
   pinterestBtn.setAttribute(
     "href",
     `https://pinterest.com/pin/create/bookmarklet/?media=${postImg}&url=${PostUrl}&description=${postTitle}`
-  );
+  );}
+  if(whatsappBtn){
   whatsappBtn.setAttribute(
     "href",
     `https://api.whatsapp.com/send?text=${postTitle} ${PostUrl}`
-  );
+  );}
 }
 share();
